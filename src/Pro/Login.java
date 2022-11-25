887292b20ecb7ca44bb3915c993f1fb88e22f836
Stefan <Stefan.lajtar@students.ittralee.ie>
2022-11-25 10:34:52 +0000

    Fourth

--- src/Pro/Login.java
+++ src/Pro/Login.java
@@ -5,60 +5,60 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Login extends JFrame implements ActionListener {
+public class Login extends JFrame  {
     private JTextField usernameText;
-    private JPanel loginScreen;
+    private JPanel panel1;
     private JPasswordField passwordText;
     private JButton loginButton;
-    private JFrame frame;
 
     private String password;
     private String username;
 
 
+
     public Login() {
 
-        frame = new JFrame("Login");
-        frame.setPreferredSize(new Dimension(250, 250));
-        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame.setResizable(false);
+        setTitle("Login");
+        setPreferredSize(new Dimension(250, 250));
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setContentPane(panel1);
+        setVisible(true);
+        pack();
 
 
-        frame.add(loginScreen);
-
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
 
-    }
+                if (e.getSource() == loginButton || e.getSource() == usernameText || e.getSource() == passwordText) {
+                    username = usernameText.getText();
+                    password = String.valueOf(passwordText.getPassword());
+                    if (username.equals("admin") && (password.equals("admin"))) {
 
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==loginButton || e.getSource()==usernameText || e.getSource()==passwordText){
-            username = usernameText.getText();
-            password = String.valueOf(passwordText.getPassword());
-             if(username.equals("admin") && (password.equals("admin"))) {
+                        new RentalSystem();
+                        setVisible(true);
+                        dispose();
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Incorrect password or username!", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
 
 
-                 RentalSystem r = new RentalSystem();
-                 r.setVisible(true);
-                 dispose();
-             }
+                }
+            }
+    });
 
-           else{
-               JOptionPane.showMessageDialog(null,"Incorrect password or username!","Error",JOptionPane.ERROR_MESSAGE);
-             }
-        }
-
-
-
-    }
 
 
 
 }
 
+    public static void main(String[] args) {
+        Login gui = new Login();
+    }
+}
 
 
